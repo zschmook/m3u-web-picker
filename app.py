1c8064c5b65d4aad84bb359e8c7090bd513d2829
Zachary Schmook <zachary.schmook@gmail.com>
2026-08-11 23:01:24 -0400

    Load provider cleanup styling

--- app.py
+++ app.py
@@ -19,17 +19,18 @@ def index():
     html = render_template("index.html")
     html = html.replace(
         "</head>",
-        '<link rel="stylesheet" href="/static/css/experiments_ui.css?v=ui-refactor-4">\n'
-        '<link rel="stylesheet" href="/static/css/ui_refactor.css?v=ui-refactor-4">\n'
-        '<link rel="stylesheet" href="/static/css/ui_themes.css?v=ui-refactor-4">\n'
-        '<link rel="stylesheet" href="/static/css/ui_sections.css?v=ui-refactor-4">\n</head>',
+        '<link rel="stylesheet" href="/static/css/experiments_ui.css?v=ui-refactor-5">\n'
+        '<link rel="stylesheet" href="/static/css/ui_refactor.css?v=ui-refactor-5">\n'
+        '<link rel="stylesheet" href="/static/css/ui_themes.css?v=ui-refactor-5">\n'
+        '<link rel="stylesheet" href="/static/css/ui_sections.css?v=ui-refactor-5">\n'
+        '<link rel="stylesheet" href="/static/css/ui_provider_cleanup.css?v=ui-refactor-5">\n</head>',
     )
     return html.replace(
         "</body>",
-        '<script src="/static/js/experiments_ui.js?v=ui-refactor-4"></script>\n'
-        '<script src="/static/js/ui_refactor.js?v=ui-refactor-4"></script>\n'
-        '<script src="/static/js/ui_themes.js?v=ui-refactor-4"></script>\n'
-        '<script src="/static/js/ui_sections.js?v=ui-refactor-4"></script>\n'
+        '<script src="/static/js/experiments_ui.js?v=ui-refactor-5"></script>\n'
+        '<script src="/static/js/ui_refactor.js?v=ui-refactor-5"></script>\n'
+        '<script src="/static/js/ui_themes.js?v=ui-refactor-5"></script>\n'
+        '<script src="/static/js/ui_sections.js?v=ui-refactor-5"></script>\n'
         '<script src="/static/js/hdhr_ui.js?v=hdhr-support-toggle-1"></script>\n</body>',
     )
 
