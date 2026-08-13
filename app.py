fcecea5e4afc121b3b7dc3f20612c447428b1aa5
Zachary Schmook <zachary.schmook@gmail.com>
2026-08-13 18:23:00 -0400

    Bump TV Guide asset after removing theme picker

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def guide():
         '<script src="/static/js/guide_programmes.js?v=tv-grid-4"></script>\n'
         '<script src="/static/js/guide_roku_button.js?v=roku-button-2"></script>\n'
         '<script src="/static/js/ui_resilient_images.js?v=ui-refactor-9"></script>\n'
-        '<script src="/static/js/guide_back_button.js?v=guide-back-2"></script>\n</body>',
+        '<script src="/static/js/guide_back_button.js?v=guide-back-3"></script>\n</body>',
     )
 
 
