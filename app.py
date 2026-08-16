d876eb938342dc3a5ba15e139754f4433edf0e0e
Zachary Schmook <zachary.schmook@gmail.com>
2026-08-16 04:07:07 -0400

    Remove direct TS URL copy control

--- app.py
+++ app.py
@@ -116,8 +116,7 @@ def guide():
         '<script src="/static/js/guide_event_logo_bridge.js?v=event-logo-2"></script>\n'
         '<script src="/static/js/guide_roku_button.js?v=roku-button-2"></script>\n'
         '<script src="/static/js/ui_resilient_images.js?v=logo-registry-1"></script>\n'
-        '<script src="/static/js/guide_back_button.js?v=guide-back-3"></script>\n'
-        '<script src="/static/js/guide_debug_ts.js?v=debug-ts-1"></script>\n</body>',
+        '<script src="/static/js/guide_back_button.js?v=guide-back-3"></script>\n</body>',
     )
 
 
