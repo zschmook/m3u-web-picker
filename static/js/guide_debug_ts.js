f0beaeca82d8de07a426af4cbb130c1fd708271b
Zachary Schmook <zachary.schmook@gmail.com>
2026-08-16 04:07:39 -0400

    Remove obsolete TS copy script

--- static/js/guide_debug_ts.js
+++
@@ -1,6 +0,0 @@
-(() => {
-  "use strict";
-
-  // Direct MPEG-TS stream URLs are intentionally not exposed in the Guide UI.
-  // The internal debug routes remain available for development plumbing.
-})();
